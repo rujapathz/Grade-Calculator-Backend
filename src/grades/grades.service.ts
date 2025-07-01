110b45bab9bac5f2894b9113c0b76f2568b98bcc
Rujapa <rujapathz@gmail.com>
2025-07-01 11:17:28 +0700

    preparing to git flow first time

--- src/grades/grades.service.ts
+++ src/grades/grades.service.ts
@@ -4,17 +4,19 @@ import { UpdateGradeDto } from './dto/update-grade.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryFailedError, Repository } from 'typeorm';
 import { GradeEntity } from './entities/grade.entity';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class GradesService {
 
   constructor(@InjectRepository(GradeEntity) private readonly gradeRepository: Repository<GradeEntity>)
   {}
-
-
   async create(createGradeDto: CreateGradeDto): Promise<GradeEntity> {
     try {
-
+      const extistName = await this.gradeRepository.findOneBy({ name: createGradeDto.name});
+      if (extistName) {
+        throw new BadRequestException(`${createGradeDto.name} is already taken.`);
+      }
       const dataUser = this.gradeRepository.create(createGradeDto)
       const createResult = await this.gradeRepository.save(dataUser)
       return createResult;
@@ -40,11 +42,19 @@ export class GradesService {
 }
 
   async update(id: number, updateUserDto: UpdateGradeDto): Promise<GradeEntity | null> { 
-    await this.gradeRepository.update(id, updateUserDto)
-    const dataUserUpdate = await this.gradeRepository.findOneBy({id});
-    return dataUserUpdate;
-  }
-
+    try {
+     if (updateUserDto.name) {
+      const existing = await this.gradeRepository.findOne({ where: { name: updateUserDto.name } });
+      if (existing && existing.id !== id) {
+        throw new BadRequestException(`${updateUserDto.name} is already taken.`);
+      }
+    }
+    await this.gradeRepository.update(id, updateUserDto);
+    return this.gradeRepository.findOneBy({ id });
+  } catch (error) {
+    throw error;
+      }
+    }
   remove(id: number) {
     return this.gradeRepository.delete(id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
+
   app.useGlobalPipes(
     new ValidationPipe({
       // disableErrorMessages: true,
       whitelist: true,
       transform: true,
     }));
+
+    // app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
